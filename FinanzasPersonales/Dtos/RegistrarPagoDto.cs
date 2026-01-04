a1e6fc7768d52253885d0aead921de4854ebcf23
jojoa8209 <jal82tuc@gmail.com>
2026-01-04 19:34:30 -0300

    Feat: Creación de RegistrarPagoDto para manejo de pagos

---
+++ FinanzasPersonales/Dtos/RegistrarPagoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FinanzasPersonales.API.Dtos
+{
+    public class RegistrarPagoDto
+    { public int DeudaId { get; set; }
+        public decimal Monto { get; set; }
+
+        public string MedioPago { get; set; } = string.Empty;
+
+        public DateTime FechaDePago { get; set; } = DateTime.Now;
+    }
+}
